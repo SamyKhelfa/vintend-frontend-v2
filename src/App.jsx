dba38182ad98008ee49ef8e0f78010a449f32375
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-08-29 14:57:00 +0200

    add stripe

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cookies from "js-cookie";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./assets/components/CheckoutForm";
 
 //import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -9,6 +12,7 @@ import Signup from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 //import Publish from "./pages/Publish";
 import Offer from "./pages/Offer";
+
 //import Payment from "./pages/Payment";
 
 //import { library } from "@fortawesome/fontawesome-svg-core";
@@ -20,6 +24,9 @@ function App() {
   const [sortPrice, setSortPrice] = useState(false);
   const [fetchRangeValues, setFetchRangeValues] = useState([0, 10000]);
   const [search, setSearch] = useState("");
+  const stripePromise = loadStripe(
+    "pk_test_51NkQ5CEcGykLbOWEqDxX2ry5mdDoAb54zYhmHiTOsJPvcYQPuJ98JdBLpNnOfJgwOkQUTuEIBiw3uNOgzYIwLA2000b6ds2RMS"
+  );
 
   const setUser = (token) => {
     if (token) {
@@ -55,6 +62,9 @@ function App() {
         <Route path="/offer/:id" element={<Offer />} />
         {/* <Route path="/payment" element={<Payment />} /> */}
         <Route path="/signin" element={<SignIn />} />
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
       </Routes>
     </Router>
   );
